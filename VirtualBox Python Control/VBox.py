6069b789d5d0e7db35cb84fdada00acf9bbc7390
bill <wherhard@student.rtc.edu>
2019-02-20 23:41:02 -0800

    Finally got dependencies resolved. Threading errors out on vbox control, will figure that out later, based on initial research, multiprocessing might be better than thread for this application. So far, so good on vbox control.

--- VirtualBox Python Control/VBox.py
+++ VirtualBox Python Control/VBox.py
@@ -1,11 +1,18 @@
 # Bill Erhard
-# RTC CNT 150
+# RTC CNT 350
 # Controls VirtualBox using Python
+
 import virtualbox
 
-
 def clone_vm(vm_os):
+    #from time import sleep
     print(vm_os)
+    vm = vm_os
+    vbox = virtualbox.VirtualBox()
+    for machine in vbox.machines:
+        print(machine.name)
+    print(get_ip(vm))
+    #sleep(3) # This checks that threads are async. 3 secs total sleep good, 3*num_threads bad.
     pass
 
 
@@ -22,14 +29,12 @@ def main():
 
     threads = []
     for i in range(vm_create_number):
-        t = Thread(target=clone_vm, args=(vm_os,))
-        threads.append(t)
-        t.start()
-    for t in threads:
-        t.join()
-
-    for vm in threads:
-        print(get_ip(vm))
+        #t = Thread(target=clone_vm, args=(vm_os,))
+        #threads.append(t)
+        #t.start()
+        clone_vm(vm_os)
+   # for t in threads:
+     #   t.join()
 
 
 if __name__ == '__main__':
